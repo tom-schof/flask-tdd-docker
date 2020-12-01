360d96e8f8b648393900f76a7320cfc265ecd83e
Thomas Schofield <40145969+tom-schof@users.noreply.github.com>
2020-12-01 09:46:57 -0500

    docker setup

--- flask-tdd-docker/src/__init__.py
+++ flask-tdd-docker/src/__init__.py
@@ -1,18 +1,23 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+import sys
 
 app = Flask(__name__)
 
 api = Api(app)
 
 #set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 class Ping(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'ass nuts!'
+            'message': 'Pong!'
         }
 
+# print(app.config, file=sys.stderr)
 api.add_resource(Ping, '/ping')
